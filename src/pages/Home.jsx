4169ba8bd0ebbdb36d2463554d583af2d39c5051
Mansi <mansins168@gmail.com>
2023-12-31 23:33:42 +0530

    some changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import {Suspense, useState} from 'react'
 import {Canvas} from '@react-three/fiber';
 import Loader from '../components/Loader';
-import Island from '../models/island';
+import Island from '../models/Island';
 import Sky from '../models/Sky';
 import Bird from '../models/Bird';
 import Plane from '../models/Plane';
