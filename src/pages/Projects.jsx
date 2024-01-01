0728984054d407be5a84db24f61d5163a0cae6db
Mansi <mansins168@gmail.com>
2024-01-01 22:59:58 +0530

    changes

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { projects } from '../constants'
 import { Link } from 'react-router-dom'
 import CTA from '../components/CTA'
+import { arrow } from '../assets/icons'
 
 const Projects = () => {
   return (
